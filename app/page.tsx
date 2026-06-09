38951b20554be1a4a263c6ac11ed2d17081cf544
Curtis Mitchell <c.mitchell@bloomworks.digital>
2026-06-09 15:19:31 -0700

    Biome Action changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,3 @@
 export default function Page() {
-  return <h1
-    className="text-3xl font-bold underline">
-      Home
-  </h1>;
+  return <h1 className="text-3xl font-bold underline">Home</h1>;
 }
